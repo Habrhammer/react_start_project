4d141f42822e574f95a617bad6e56ba83972337c
Habrhammer <wsuxvnfhpquz@mail.ru>
2021-12-06 19:11:09 +0300

    React.lazy, React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
-import { Route, withRouter } from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import "./App.css";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import Preloader from "./components/common/Preloader/Preloader";
-import { connect } from "react-redux";
+import { connect, Provider } from "react-redux";
 import { compose } from "redux";
 import { initializeApp } from "./redux/app-reducer";
+import {withSuspense} from "./hoc/withSuspense";
+import store from "./redux/redux-store";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -28,15 +33,13 @@ class App extends React.Component {
 
         <Route
           path="/dialogs/:userId?"
-          render={() => {
-            return <DialogsContainer />;
-          }}
+          render={withSuspense(DialogsContainer)
+          }
         />
         <Route
           path="/profile/:userId?"
-          render={() => {
-            return <ProfileContainer />;
-          }}
+          render={withSuspense(ProfileContainer)
+          }
         />
         <Route
           path="/users"
@@ -59,7 +62,19 @@ const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
 });
 
-export default compose(
-  // withRouter,
-  connect(mapStateToProps, { initializeApp })
-)(App);
+
+let AppContainer = compose(
+  withRouter,
+  connect(mapStateToProps, {initializeApp}))(App);
+
+
+
+const SocialNetworkApp = (props) => {
+  return <BrowserRouter>
+       <Provider store={store}>
+           <AppContainer />
+       </Provider>
+   </BrowserRouter>
+}
+
+export default SocialNetworkApp;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import store from "./redux/redux-store";
 import "./index.css";
-import { Provider } from "react-redux";
-
-ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById("root")
-);
+import SocialNetworkApp from "./App";
 
 
+ReactDOM.render(<SocialNetworkApp/>, document.getElementById('root'));
